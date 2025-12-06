51de2f51b387fd637866e97f142f9bee27dba1b4
Rodrigo Torres <rtmr@cesar.school>
2025-12-06 12:59:46 -0300

    a parada esta completa e rodando

--- app.py
+++ app.py
@@ -620,4 +620,5 @@ def download_pdf_api(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # Altere app.run() para usar host='0.0.0.0'
+    app.run(host='0.0.0.0', debug=True, port=5001)
